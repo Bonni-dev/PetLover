05e9a93339c60d2d7e8c5d22c1dff6b38c66d066
Bononi <53324114+Bonnivaldo@users.noreply.github.com>
2024-04-06 17:44:50 -0300

    move classes

--- app/src/main/java/com/example/petlover/ui/view/UserInputView.kt
+++
@@ -1,8 +0,0 @@
-package com.example.petlover.ui.view
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun UserInputScreen(){
-
-}
--- app/src/main/java/com/example/petlover/ui/view/WelcomeView.kt
+++
@@ -1,6 +0,0 @@
-package com.example.petlover.ui.view
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun WelcomeScreen(){}
