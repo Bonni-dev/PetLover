89cc49af846c0a48502ff1b9221e8eee5ba8afd3
Bononi <53324114+Bonnivaldo@users.noreply.github.com>
2024-04-07 11:18:17 -0300

    Add SplashScreen

--- app/src/main/java/com/example/petlover/presentation/shared/MainActivity.kt
+++ app/src/main/java/com/example/petlover/presentation/shared/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.petlover.presentation.ui.theme.PetLoverTheme
 import com.example.petlover.navigation.PetLoverNavigationGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             PetLoverTheme {
                 PetLoverApp()
